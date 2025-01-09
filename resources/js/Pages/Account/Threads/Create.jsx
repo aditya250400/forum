f396757525de065588fea51dfd2070b8ef78efb9
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-01-09 15:11:43 +0700

    fix notif when creating thread

--- resources/js/Pages/Account/Threads/Create.jsx
+++ resources/js/Pages/Account/Threads/Create.jsx
@@ -8,30 +8,35 @@ import MainLayout from "@/Layouts/MainLayout";
 import { Head, useForm, usePage } from "@inertiajs/react";
 import { IconPencilPlus } from "@tabler/icons-react";
 import { toast } from "react-hot-toast";
-
 export default function Create() {
+    // define construct props
     const { tags } = usePage().props;
 
-    const { data, setData, post, transform, errors, processing } = useForm({
+    // define useform helper inertia
+    const { data, setData, post, transform, errors } = useForm({
         title: "",
         content: "",
         tagsData: [],
         description: "",
     });
 
+    // get content value
     const setContent = (value) => {
         setData("content", value);
     };
 
+    // get tags value
     const setTags = (value) => {
         setData("tagsData", value);
     };
 
+    // transform data before submit
     transform((data) => ({
         ...data,
         tags: data.tagsData.map((tag) => tag.label),
     }));
 
+    // define method submit
     function submit(e) {
         e.preventDefault();
 
@@ -48,66 +53,66 @@ export default function Create() {
             },
         });
     }
+
     return (
         <>
             <Head title="Create Threads" />
-            <MainLayout>
-                <div className="flex justify-center gap-4">
-                    <div className="w-full sm:w-8/12">
-                        <Card
-                            title="Create New Thread"
-                            icon={
-                                <IconPencilPlus
-                                    strokeWidth={"1.5"}
-                                    className="w-5 h-5"
-                                />
-                            }
-                            action={submit}
-                            btnTitle="Create Threads"
-                            href={"/threads"}
-                            processing={processing}
-                        >
-                            <div className="mb-5">
-                                <Input
-                                    label={"Title"}
-                                    value={data.title}
-                                    onChange={(e) =>
-                                        setData("title", e.target.value)
-                                    }
-                                    errors={errors.title}
-                                />
-                            </div>
-                            <div className="mb-5">
-                                <Textarea
-                                    label={"Description"}
-                                    value={data.description}
-                                    onChange={(e) =>
-                                        setData("description", e.target.value)
-                                    }
-                                    errors={errors.description}
-                                />
-                            </div>
-                            <div className="mb-5">
-                                <MultiSelect
-                                    label={"Tags"}
-                                    options={tags}
-                                    value={data.tagsData}
-                                    onChange={setTags}
-                                    errors={errors.tags}
-                                />
-                            </div>
-                            <div className="mb-5">
-                                <MarkdownEditor
-                                    label={"Content"}
-                                    value={data.content}
-                                    onChange={setContent}
-                                    errors={errors.content}
-                                />
-                            </div>
-                        </Card>
-                    </div>
+            <div className="flex justify-center gap-4">
+                <div className="w-full sm:w-8/12">
+                    <Card
+                        title={"Create New Threads"}
+                        icon={
+                            <IconPencilPlus
+                                strokeWidth={"1.5"}
+                                className="w-5 h-5"
+                            />
+                        }
+                        action={submit}
+                        btnTitle={"Create Threads"}
+                        href={"/threads"}
+                    >
+                        <div className="mb-5">
+                            <Input
+                                label={"Title"}
+                                value={data.title}
+                                onChange={(e) =>
+                                    setData("title", e.target.value)
+                                }
+                                errors={errors.title}
+                            />
+                        </div>
+                        <div className="mb-5">
+                            <Textarea
+                                label={"Description"}
+                                value={data.description}
+                                onChange={(e) =>
+                                    setData("description", e.target.value)
+                                }
+                                errors={errors.description}
+                            />
+                        </div>
+                        <div className="mb-5">
+                            <MultiSelect
+                                label={"Tags"}
+                                options={tags}
+                                value={data.tagsData}
+                                onChange={setTags}
+                                errors={errors.tags}
+                            />
+                        </div>
+                        <div className="mb-5">
+                            <MarkdownEditor
+                                label={"Content"}
+                                value={data.content}
+                                onChange={setContent}
+                                errors={errors.content}
+                            />
+                        </div>
+                    </Card>
                 </div>
-            </MainLayout>
+            </div>
         </>
     );
 }
+
+Create.layout = (page) => <MainLayout children={page} />;
